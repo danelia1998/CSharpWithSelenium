a7faf3ffaca1295bb3e6aed08c827669a2c4b2b8
david danelia <davit.danelia.1@btu.edu.ge>
2021-01-21 21:22:40 +0400

    Creating Project Using PAckSharp, NUnit 3 using command Palette- dotnet build, packsharp selenium bootstrap

---
+++ Royale.Tests/CardTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Royale.Tests
+{
+    public class CardTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Ice_Spirit_Is_On_Cards_Page()
+        {
+            Assert.Pass();
+        }
+    }
+}
